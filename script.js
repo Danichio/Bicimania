1a60fc97fb778be720247e2926f20fb6d74dd4ee
Danichio <130368300+Danichio@users.noreply.github.com>
2023-04-12 22:03:14 -0300

    datos form

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
+// // referencia de los campos
+// const nombre = document.getElementById("nombre")
+// const correo = document.getElementById("correo")
+// const modelo = document.getElementById("modelo")
+// const mensaje = document.getElementById("mensaje")
+
+// const form = document.querySelector('form');
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   // Envía la solicitud de precio por correo electrónico utilizando una librería de terceros como SendGrid o Nodemailer
+  
+//   // Limpia el formulario
+//   form.reset();
+
+  
+//   // Muestra un mensaje de confirmación al usuario
+//   alert('Gracias por enviar tu solicitud de precio. Nos pondremos en contacto contigo pronto.');
+// });
+
+
+
+
+// function handlesumit (){
+// console.log(nombre?.value, correo?.value, modelo?.value, mensaje?.value)}
+
+
+// // {
+// //   console.log("
+// //     datos para procesar el Formulario
+// //     ${nombre}
+// //     ${correo}
+// //     ${modelo}
+// //     ${mensaje}
+// // //     "
+    
+
+// //     )
+// // }
+
 const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
+
+form.addEventListener('submit', capturadatos)
+
+function capturadatos (e){
   e.preventDefault();
-  
-  // Envía la solicitud de precio por correo electrónico utilizando una librería de terceros como SendGrid o Nodemailer
-  
-  // Limpia el formulario
-  form.reset();
-  
-  // Muestra un mensaje de confirmación al usuario
-  alert('Gracias por enviar tu solicitud de precio. Nos pondremos en contacto contigo pronto.');
-});
+    const name=document.getElementById("nombre");
+    const mail=document.getElementById("correo");
+    const model=document.getElementById("modelo");
+    const medida=document.getElementById("mensaje");
+    console.log (name?.value, mail?.value, model?.value, medida?.value);
+}
